6399d8bb50266ecfde8a68f1047562045ebf0d6a
rohan sahu <rohansahu1212@gmail.com>
2021-08-23 13:16:39 +0530

    jarvis here

---
+++ whatsapp.py
@@ -0,0 +1,15 @@
+import pywhatkit as pw
+import datetime
+import csv
+import os
+#hour = int(datetime.datetime.now().hour)
+#minn = int(datetime.datetime.now().minute) + 2
+#pw.sendwhatmsg("+919839055102","hello papa",hour,minn) 
+###conn="sagar"
+"""with open ("contact.csv") as csv_file:
+    csv_file = csv.reader(csv_file)
+    for line in csv_file:
+        if conn in line:
+            recc=line[1]
+            print(recc)"""
+os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Brave")
